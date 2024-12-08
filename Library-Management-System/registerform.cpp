ced0abc6b45305e42088d6e091fc82f3c3ca4c31
Hashir Asad <hashirbinasad@gmail.com>
2024-12-08 14:01:40 +0500

    Test User Registration File
    - Implemented feature to store user credentials from RegisterForm

--- Library-Management-System/registerform.cpp
+++ Library-Management-System/registerform.cpp
@@ -1,5 +1,8 @@
 #include "registerform.h"
 #include "ui_registerform.h"
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
 
 RegisterForm::RegisterForm(QWidget *parent)
     : QWidget(parent)
@@ -15,7 +18,32 @@ RegisterForm::~RegisterForm()
 
 void RegisterForm::on_RegisterButton_clicked()
 {
-    qDebug() << "Registered!";
+    QString username = ui->UsernameField->text();
+    QString email = ui->EmailTextField->text();
+    QString password = ui->PasswordField->text();
+
+
+    if (username.isEmpty() || email.isEmpty() || password.isEmpty()) {
+        qDebug() << "Please fill all fields!";
+        return;
+    }
+
+
+    QFile file("users.txt");
+    if (!file.open(QIODevice::Append | QIODevice::Text)) {
+        qDebug() << "Failed to open file for writing.";
+        return;
+    }
+
+    QTextStream out(&file);
+    out << username << "," << email << "," << password << "\n";
+
+    file.close();
+    qDebug() << "Registration successful!";
+
+    ui->UsernameField->clear();
+    ui->EmailTextField->clear();
+    ui->PasswordField->clear();
 }
 
 void RegisterForm::on_CancelButton_clicked()
