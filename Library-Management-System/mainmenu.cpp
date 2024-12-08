d451ac11fb57ec2db7fdb994b6c8b6d4b75efa07
Malik Daniyal Ahmad <146337576+danisensei@users.noreply.github.com>
2024-12-08 19:46:45 +0500

    ...

--- Library-Management-System/mainmenu.cpp
+++ Library-Management-System/mainmenu.cpp
@@ -13,52 +13,52 @@ mainmenu::~mainmenu()
     delete ui;
 }
 
-void mainmenu :: on_adduserbutton_clicked();
+void mainmenu :: on_adduserbutton_clicked()
 {
-
+qDebug() << "add user clicked";
 }
 
-void mainmenu :: on_deleteuserbutton_clicked();
+void mainmenu :: on_deleteuserbutton_clicked()
 {
-
+qDebug() << "delete user";
 }
 
-void mainmenu :: on_edituserbutton_clicked();
+void mainmenu :: on_edituserbutton_clicked()
 {
-
+qDebug() << "edit user";
 }
 
-void mainmenu :: on_viewusersbutton_clicked();
+void mainmenu :: on_viewusersbutton_clicked()
 {
-
+qDebug() << "viewusers!";
 }
 
-void mainmenu :: on_addbookbutton_clicked();
+void mainmenu :: on_addbookbutton_clicked()
 {
-
+qDebug() << "addbooks!";
 }
 
-void mainmenu :: on_deletebookbutton_clicked();
+void mainmenu :: on_deletebookbutton_clicked()
 {
-
+qDebug() << "deletebook!";
 }
 
-void mainmenu :: on_editbookbutton_clicked();
+void mainmenu :: on_editbookbutton_clicked()
 {
-
+qDebug() << "editbook!";
 }
 
-void mainmenu :: on_viewbooksbutton_clicked();
+void mainmenu :: on_viewbooksbutton_clicked()
 {
-
+qDebug() << "Invalid credentials!";
 }
 
-void mainmenu :: on_issuebookbutton_clicked();
+void mainmenu :: on_issuebookbutton_clicked()
 {
-
+qDebug() << "issuebook!";
 }
 
-void mainmenu :: on_returnbookbutton_clicked();
+void mainmenu :: on_returnbookbutton_clicked()
 {
-
+qDebug() << "returnbook!";
 }
